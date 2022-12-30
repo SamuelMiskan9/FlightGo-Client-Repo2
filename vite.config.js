9682cfc16733a8f08efd4a08a64e9cc4f96648a2
Syahri Rhamadhan <2001020038@student.umrah.ac.id>
2022-12-30 15:13:31 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  mode: 'spa',
-  rollupOptions: {
-    input: './index.html',
-  },
-  plugins: [react()],
+  plugins: [react()]
 })
